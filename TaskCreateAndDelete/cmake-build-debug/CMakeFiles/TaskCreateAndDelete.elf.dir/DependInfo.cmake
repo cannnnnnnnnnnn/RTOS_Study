03563aaaa67bcc6ca1417d4862585be6f7f7b189
超级小星星 <62045849+cannnnnnnnnnnn@users.noreply.github.com>
2023-03-29 13:53:52 +0800

    project init

---
+++ TaskCreateAndDelete/cmake-build-debug/CMakeFiles/TaskCreateAndDelete.elf.dir/DependInfo.cmake
@@ -0,0 +1,65 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "ASM"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_ASM
+  "D:/Clion_STM32/CLionProjects/OS/RTOS_Study/TaskCreateAndDelete/Core/Startup/startup_stm32g474retx.s" "D:/Clion_STM32/CLionProjects/OS/RTOS_Study/TaskCreateAndDelete/cmake-build-debug/CMakeFiles/TaskCreateAndDelete.elf.dir/Core/Startup/startup_stm32g474retx.s.obj"
+  )
+set(CMAKE_ASM_COMPILER_ID "GNU")
+
+# Preprocessor definitions for this target.
+set(CMAKE_TARGET_DEFINITIONS_ASM
+  "DEBUG"
+  "STM32G474xx"
+  "USE_HAL_DRIVER"
+  )
+
+# The include file search paths:
+set(CMAKE_ASM_TARGET_INCLUDE_PATH
+  "D:/Clion_STM32/CLionProjects/OS/RTOS_Study/TaskCreateAndDelete/Core/Inc"
+  "D:/Clion_STM32/CLionProjects/OS/RTOS_Study/TaskCreateAndDelete/Drivers/STM32G4xx_HAL_Driver/Inc"
+  "D:/Clion_STM32/CLionProjects/OS/RTOS_Study/TaskCreateAndDelete/Drivers/STM32G4xx_HAL_Driver/Inc/Legacy"
+  "D:/Clion_STM32/CLionProjects/OS/RTOS_Study/TaskCreateAndDelete/Drivers/CMSIS/Device/ST/STM32G4xx/Include"
+  "D:/Clion_STM32/CLionProjects/OS/RTOS_Study/TaskCreateAndDelete/Drivers/CMSIS/Include"
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "D:/Clion_STM32/CLionProjects/OS/RTOS_Study/TaskCreateAndDelete/Core/Src/gpio.c" "CMakeFiles/TaskCreateAndDelete.elf.dir/Core/Src/gpio.c.obj" "gcc" "CMakeFiles/TaskCreateAndDelete.elf.dir/Core/Src/gpio.c.obj.d"
+  "D:/Clion_STM32/CLionProjects/OS/RTOS_Study/TaskCreateAndDelete/Core/Src/main.c" "CMakeFiles/TaskCreateAndDelete.elf.dir/Core/Src/main.c.obj" "gcc" "CMakeFiles/TaskCreateAndDelete.elf.dir/Core/Src/main.c.obj.d"
+  "D:/Clion_STM32/CLionProjects/OS/RTOS_Study/TaskCreateAndDelete/Core/Src/stm32g4xx_hal_msp.c" "CMakeFiles/TaskCreateAndDelete.elf.dir/Core/Src/stm32g4xx_hal_msp.c.obj" "gcc" "CMakeFiles/TaskCreateAndDelete.elf.dir/Core/Src/stm32g4xx_hal_msp.c.obj.d"
+  "D:/Clion_STM32/CLionProjects/OS/RTOS_Study/TaskCreateAndDelete/Core/Src/stm32g4xx_it.c" "CMakeFiles/TaskCreateAndDelete.elf.dir/Core/Src/stm32g4xx_it.c.obj" "gcc" "CMakeFiles/TaskCreateAndDelete.elf.dir/Core/Src/stm32g4xx_it.c.obj.d"
+  "D:/Clion_STM32/CLionProjects/OS/RTOS_Study/TaskCreateAndDelete/Core/Src/syscalls.c" "CMakeFiles/TaskCreateAndDelete.elf.dir/Core/Src/syscalls.c.obj" "gcc" "CMakeFiles/TaskCreateAndDelete.elf.dir/Core/Src/syscalls.c.obj.d"
+  "D:/Clion_STM32/CLionProjects/OS/RTOS_Study/TaskCreateAndDelete/Core/Src/sysmem.c" "CMakeFiles/TaskCreateAndDelete.elf.dir/Core/Src/sysmem.c.obj" "gcc" "CMakeFiles/TaskCreateAndDelete.elf.dir/Core/Src/sysmem.c.obj.d"
+  "D:/Clion_STM32/CLionProjects/OS/RTOS_Study/TaskCreateAndDelete/Core/Src/system_stm32g4xx.c" "CMakeFiles/TaskCreateAndDelete.elf.dir/Core/Src/system_stm32g4xx.c.obj" "gcc" "CMakeFiles/TaskCreateAndDelete.elf.dir/Core/Src/system_stm32g4xx.c.obj.d"
+  "D:/Clion_STM32/CLionProjects/OS/RTOS_Study/TaskCreateAndDelete/Core/Src/usart.c" "CMakeFiles/TaskCreateAndDelete.elf.dir/Core/Src/usart.c.obj" "gcc" "CMakeFiles/TaskCreateAndDelete.elf.dir/Core/Src/usart.c.obj.d"
+  "D:/Clion_STM32/CLionProjects/OS/RTOS_Study/TaskCreateAndDelete/Drivers/STM32G4xx_HAL_Driver/Src/stm32g4xx_hal.c" "CMakeFiles/TaskCreateAndDelete.elf.dir/Drivers/STM32G4xx_HAL_Driver/Src/stm32g4xx_hal.c.obj" "gcc" "CMakeFiles/TaskCreateAndDelete.elf.dir/Drivers/STM32G4xx_HAL_Driver/Src/stm32g4xx_hal.c.obj.d"
+  "D:/Clion_STM32/CLionProjects/OS/RTOS_Study/TaskCreateAndDelete/Drivers/STM32G4xx_HAL_Driver/Src/stm32g4xx_hal_cortex.c" "CMakeFiles/TaskCreateAndDelete.elf.dir/Drivers/STM32G4xx_HAL_Driver/Src/stm32g4xx_hal_cortex.c.obj" "gcc" "CMakeFiles/TaskCreateAndDelete.elf.dir/Drivers/STM32G4xx_HAL_Driver/Src/stm32g4xx_hal_cortex.c.obj.d"
+  "D:/Clion_STM32/CLionProjects/OS/RTOS_Study/TaskCreateAndDelete/Drivers/STM32G4xx_HAL_Driver/Src/stm32g4xx_hal_dma.c" "CMakeFiles/TaskCreateAndDelete.elf.dir/Drivers/STM32G4xx_HAL_Driver/Src/stm32g4xx_hal_dma.c.obj" "gcc" "CMakeFiles/TaskCreateAndDelete.elf.dir/Drivers/STM32G4xx_HAL_Driver/Src/stm32g4xx_hal_dma.c.obj.d"
+  "D:/Clion_STM32/CLionProjects/OS/RTOS_Study/TaskCreateAndDelete/Drivers/STM32G4xx_HAL_Driver/Src/stm32g4xx_hal_dma_ex.c" "CMakeFiles/TaskCreateAndDelete.elf.dir/Drivers/STM32G4xx_HAL_Driver/Src/stm32g4xx_hal_dma_ex.c.obj" "gcc" "CMakeFiles/TaskCreateAndDelete.elf.dir/Drivers/STM32G4xx_HAL_Driver/Src/stm32g4xx_hal_dma_ex.c.obj.d"
+  "D:/Clion_STM32/CLionProjects/OS/RTOS_Study/TaskCreateAndDelete/Drivers/STM32G4xx_HAL_Driver/Src/stm32g4xx_hal_exti.c" "CMakeFiles/TaskCreateAndDelete.elf.dir/Drivers/STM32G4xx_HAL_Driver/Src/stm32g4xx_hal_exti.c.obj" "gcc" "CMakeFiles/TaskCreateAndDelete.elf.dir/Drivers/STM32G4xx_HAL_Driver/Src/stm32g4xx_hal_exti.c.obj.d"
+  "D:/Clion_STM32/CLionProjects/OS/RTOS_Study/TaskCreateAndDelete/Drivers/STM32G4xx_HAL_Driver/Src/stm32g4xx_hal_flash.c" "CMakeFiles/TaskCreateAndDelete.elf.dir/Drivers/STM32G4xx_HAL_Driver/Src/stm32g4xx_hal_flash.c.obj" "gcc" "CMakeFiles/TaskCreateAndDelete.elf.dir/Drivers/STM32G4xx_HAL_Driver/Src/stm32g4xx_hal_flash.c.obj.d"
+  "D:/Clion_STM32/CLionProjects/OS/RTOS_Study/TaskCreateAndDelete/Drivers/STM32G4xx_HAL_Driver/Src/stm32g4xx_hal_flash_ex.c" "CMakeFiles/TaskCreateAndDelete.elf.dir/Drivers/STM32G4xx_HAL_Driver/Src/stm32g4xx_hal_flash_ex.c.obj" "gcc" "CMakeFiles/TaskCreateAndDelete.elf.dir/Drivers/STM32G4xx_HAL_Driver/Src/stm32g4xx_hal_flash_ex.c.obj.d"
+  "D:/Clion_STM32/CLionProjects/OS/RTOS_Study/TaskCreateAndDelete/Drivers/STM32G4xx_HAL_Driver/Src/stm32g4xx_hal_flash_ramfunc.c" "CMakeFiles/TaskCreateAndDelete.elf.dir/Drivers/STM32G4xx_HAL_Driver/Src/stm32g4xx_hal_flash_ramfunc.c.obj" "gcc" "CMakeFiles/TaskCreateAndDelete.elf.dir/Drivers/STM32G4xx_HAL_Driver/Src/stm32g4xx_hal_flash_ramfunc.c.obj.d"
+  "D:/Clion_STM32/CLionProjects/OS/RTOS_Study/TaskCreateAndDelete/Drivers/STM32G4xx_HAL_Driver/Src/stm32g4xx_hal_gpio.c" "CMakeFiles/TaskCreateAndDelete.elf.dir/Drivers/STM32G4xx_HAL_Driver/Src/stm32g4xx_hal_gpio.c.obj" "gcc" "CMakeFiles/TaskCreateAndDelete.elf.dir/Drivers/STM32G4xx_HAL_Driver/Src/stm32g4xx_hal_gpio.c.obj.d"
+  "D:/Clion_STM32/CLionProjects/OS/RTOS_Study/TaskCreateAndDelete/Drivers/STM32G4xx_HAL_Driver/Src/stm32g4xx_hal_pwr.c" "CMakeFiles/TaskCreateAndDelete.elf.dir/Drivers/STM32G4xx_HAL_Driver/Src/stm32g4xx_hal_pwr.c.obj" "gcc" "CMakeFiles/TaskCreateAndDelete.elf.dir/Drivers/STM32G4xx_HAL_Driver/Src/stm32g4xx_hal_pwr.c.obj.d"
+  "D:/Clion_STM32/CLionProjects/OS/RTOS_Study/TaskCreateAndDelete/Drivers/STM32G4xx_HAL_Driver/Src/stm32g4xx_hal_pwr_ex.c" "CMakeFiles/TaskCreateAndDelete.elf.dir/Drivers/STM32G4xx_HAL_Driver/Src/stm32g4xx_hal_pwr_ex.c.obj" "gcc" "CMakeFiles/TaskCreateAndDelete.elf.dir/Drivers/STM32G4xx_HAL_Driver/Src/stm32g4xx_hal_pwr_ex.c.obj.d"
+  "D:/Clion_STM32/CLionProjects/OS/RTOS_Study/TaskCreateAndDelete/Drivers/STM32G4xx_HAL_Driver/Src/stm32g4xx_hal_rcc.c" "CMakeFiles/TaskCreateAndDelete.elf.dir/Drivers/STM32G4xx_HAL_Driver/Src/stm32g4xx_hal_rcc.c.obj" "gcc" "CMakeFiles/TaskCreateAndDelete.elf.dir/Drivers/STM32G4xx_HAL_Driver/Src/stm32g4xx_hal_rcc.c.obj.d"
+  "D:/Clion_STM32/CLionProjects/OS/RTOS_Study/TaskCreateAndDelete/Drivers/STM32G4xx_HAL_Driver/Src/stm32g4xx_hal_rcc_ex.c" "CMakeFiles/TaskCreateAndDelete.elf.dir/Drivers/STM32G4xx_HAL_Driver/Src/stm32g4xx_hal_rcc_ex.c.obj" "gcc" "CMakeFiles/TaskCreateAndDelete.elf.dir/Drivers/STM32G4xx_HAL_Driver/Src/stm32g4xx_hal_rcc_ex.c.obj.d"
+  "D:/Clion_STM32/CLionProjects/OS/RTOS_Study/TaskCreateAndDelete/Drivers/STM32G4xx_HAL_Driver/Src/stm32g4xx_hal_tim.c" "CMakeFiles/TaskCreateAndDelete.elf.dir/Drivers/STM32G4xx_HAL_Driver/Src/stm32g4xx_hal_tim.c.obj" "gcc" "CMakeFiles/TaskCreateAndDelete.elf.dir/Drivers/STM32G4xx_HAL_Driver/Src/stm32g4xx_hal_tim.c.obj.d"
+  "D:/Clion_STM32/CLionProjects/OS/RTOS_Study/TaskCreateAndDelete/Drivers/STM32G4xx_HAL_Driver/Src/stm32g4xx_hal_tim_ex.c" "CMakeFiles/TaskCreateAndDelete.elf.dir/Drivers/STM32G4xx_HAL_Driver/Src/stm32g4xx_hal_tim_ex.c.obj" "gcc" "CMakeFiles/TaskCreateAndDelete.elf.dir/Drivers/STM32G4xx_HAL_Driver/Src/stm32g4xx_hal_tim_ex.c.obj.d"
+  "D:/Clion_STM32/CLionProjects/OS/RTOS_Study/TaskCreateAndDelete/Drivers/STM32G4xx_HAL_Driver/Src/stm32g4xx_hal_uart.c" "CMakeFiles/TaskCreateAndDelete.elf.dir/Drivers/STM32G4xx_HAL_Driver/Src/stm32g4xx_hal_uart.c.obj" "gcc" "CMakeFiles/TaskCreateAndDelete.elf.dir/Drivers/STM32G4xx_HAL_Driver/Src/stm32g4xx_hal_uart.c.obj.d"
+  "D:/Clion_STM32/CLionProjects/OS/RTOS_Study/TaskCreateAndDelete/Drivers/STM32G4xx_HAL_Driver/Src/stm32g4xx_hal_uart_ex.c" "CMakeFiles/TaskCreateAndDelete.elf.dir/Drivers/STM32G4xx_HAL_Driver/Src/stm32g4xx_hal_uart_ex.c.obj" "gcc" "CMakeFiles/TaskCreateAndDelete.elf.dir/Drivers/STM32G4xx_HAL_Driver/Src/stm32g4xx_hal_uart_ex.c.obj.d"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ TaskCreateAndDelete/cmake-build-debug/CMakeFiles/TaskCreateAndDelete.elf.dir/Drivers/STM32G4xx_HAL_Driver/Src/stm32g4xx_hal_dma_ex.c.obj.d
@@ -0,0 +1,37 @@
+CMakeFiles/TaskCreateAndDelete.elf.dir/Drivers/STM32G4xx_HAL_Driver/Src/stm32g4xx_hal_dma_ex.c.obj: \
+ D:\Clion_STM32\CLionProjects\OS\RTOS_Study\TaskCreateAndDelete\Drivers\STM32G4xx_HAL_Driver\Src\stm32g4xx_hal_dma_ex.c \
+ D:\Clion_STM32\CLionProjects\OS\RTOS_Study\TaskCreateAndDelete\Drivers\STM32G4xx_HAL_Driver\Inc/stm32g4xx_hal.h \
+ D:\Clion_STM32\CLionProjects\OS\RTOS_Study\TaskCreateAndDelete\Core\Inc/stm32g4xx_hal_conf.h \
+ D:\Clion_STM32\CLionProjects\OS\RTOS_Study\TaskCreateAndDelete\Drivers\STM32G4xx_HAL_Driver\Inc/stm32g4xx_hal_rcc.h \
+ D:\Clion_STM32\CLionProjects\OS\RTOS_Study\TaskCreateAndDelete\Drivers\STM32G4xx_HAL_Driver\Inc/stm32g4xx_hal_def.h \
+ D:\Clion_STM32\CLionProjects\OS\RTOS_Study\TaskCreateAndDelete\Drivers\CMSIS\Device\ST\STM32G4xx\Include/stm32g4xx.h \
+ D:\Clion_STM32\CLionProjects\OS\RTOS_Study\TaskCreateAndDelete\Drivers\CMSIS\Device\ST\STM32G4xx\Include/stm32g474xx.h \
+ D:\Clion_STM32\CLionProjects\OS\RTOS_Study\TaskCreateAndDelete\Drivers\CMSIS\Include/core_cm4.h \
+ d:\clion_stm32\stm32cude\gun-arm-embedded\lib\gcc\arm-none-eabi\10.3.1\include\stdint.h \
+ d:\clion_stm32\stm32cude\gun-arm-embedded\arm-none-eabi\include\stdint.h \
+ d:\clion_stm32\stm32cude\gun-arm-embedded\arm-none-eabi\include\machine\_default_types.h \
+ d:\clion_stm32\stm32cude\gun-arm-embedded\arm-none-eabi\include\sys\features.h \
+ d:\clion_stm32\stm32cude\gun-arm-embedded\arm-none-eabi\include\_newlib_version.h \
+ d:\clion_stm32\stm32cude\gun-arm-embedded\arm-none-eabi\include\sys\_intsup.h \
+ d:\clion_stm32\stm32cude\gun-arm-embedded\arm-none-eabi\include\sys\_stdint.h \
+ D:\Clion_STM32\CLionProjects\OS\RTOS_Study\TaskCreateAndDelete\Drivers\CMSIS\Include/cmsis_version.h \
+ D:\Clion_STM32\CLionProjects\OS\RTOS_Study\TaskCreateAndDelete\Drivers\CMSIS\Include/cmsis_compiler.h \
+ D:\Clion_STM32\CLionProjects\OS\RTOS_Study\TaskCreateAndDelete\Drivers\CMSIS\Include/cmsis_gcc.h \
+ D:\Clion_STM32\CLionProjects\OS\RTOS_Study\TaskCreateAndDelete\Drivers\CMSIS\Include/mpu_armv7.h \
+ D:\Clion_STM32\CLionProjects\OS\RTOS_Study\TaskCreateAndDelete\Drivers\CMSIS\Device\ST\STM32G4xx\Include/system_stm32g4xx.h \
+ D:\Clion_STM32\CLionProjects\OS\RTOS_Study\TaskCreateAndDelete\Drivers\STM32G4xx_HAL_Driver\Inc/Legacy/stm32_hal_legacy.h \
+ d:\clion_stm32\stm32cude\gun-arm-embedded\lib\gcc\arm-none-eabi\10.3.1\include\stddef.h \
+ D:\Clion_STM32\CLionProjects\OS\RTOS_Study\TaskCreateAndDelete\Drivers\STM32G4xx_HAL_Driver\Inc/stm32g4xx_hal_rcc_ex.h \
+ D:\Clion_STM32\CLionProjects\OS\RTOS_Study\TaskCreateAndDelete\Drivers\STM32G4xx_HAL_Driver\Inc/stm32g4xx_hal_gpio.h \
+ D:\Clion_STM32\CLionProjects\OS\RTOS_Study\TaskCreateAndDelete\Drivers\STM32G4xx_HAL_Driver\Inc/stm32g4xx_hal_gpio_ex.h \
+ D:\Clion_STM32\CLionProjects\OS\RTOS_Study\TaskCreateAndDelete\Drivers\STM32G4xx_HAL_Driver\Inc/stm32g4xx_hal_dma.h \
+ D:\Clion_STM32\CLionProjects\OS\RTOS_Study\TaskCreateAndDelete\Drivers\STM32G4xx_HAL_Driver\Inc/stm32g4xx_hal_dma_ex.h \
+ D:\Clion_STM32\CLionProjects\OS\RTOS_Study\TaskCreateAndDelete\Drivers\STM32G4xx_HAL_Driver\Inc/stm32g4xx_hal_cortex.h \
+ D:\Clion_STM32\CLionProjects\OS\RTOS_Study\TaskCreateAndDelete\Drivers\STM32G4xx_HAL_Driver\Inc/stm32g4xx_hal_exti.h \
+ D:\Clion_STM32\CLionProjects\OS\RTOS_Study\TaskCreateAndDelete\Drivers\STM32G4xx_HAL_Driver\Inc/stm32g4xx_hal_flash.h \
+ D:\Clion_STM32\CLionProjects\OS\RTOS_Study\TaskCreateAndDelete\Drivers\STM32G4xx_HAL_Driver\Inc/stm32g4xx_hal_flash_ex.h \
+ D:\Clion_STM32\CLionProjects\OS\RTOS_Study\TaskCreateAndDelete\Drivers\STM32G4xx_HAL_Driver\Inc/stm32g4xx_hal_flash_ramfunc.h \
+ D:\Clion_STM32\CLionProjects\OS\RTOS_Study\TaskCreateAndDelete\Drivers\STM32G4xx_HAL_Driver\Inc/stm32g4xx_hal_pwr.h \
+ D:\Clion_STM32\CLionProjects\OS\RTOS_Study\TaskCreateAndDelete\Drivers\STM32G4xx_HAL_Driver\Inc/stm32g4xx_hal_pwr_ex.h \
+ D:\Clion_STM32\CLionProjects\OS\RTOS_Study\TaskCreateAndDelete\Drivers\STM32G4xx_HAL_Driver\Inc/stm32g4xx_hal_uart.h \
+ D:\Clion_STM32\CLionProjects\OS\RTOS_Study\TaskCreateAndDelete\Drivers\STM32G4xx_HAL_Driver\Inc/stm32g4xx_hal_uart_ex.h
