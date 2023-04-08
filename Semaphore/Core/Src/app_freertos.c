e678cd6c6a1afba322e9c8fac1b76aca9e68fdb7
超级小星星 <62045849+cannnnnnnnnnnn@users.noreply.github.com>
2023-04-08 16:00:17 +0800

    二值信号量测试

--- Semaphore/Core/Src/app_freertos.c
+++ Semaphore/Core/Src/app_freertos.c
@@ -26,6 +26,8 @@
 /* Private includes ----------------------------------------------------------*/
 /* USER CODE BEGIN Includes */
 #include "stdio.h"
+#include "queue.h"
+#include "semphr.h"
 /* USER CODE END Includes */
 
 /* Private typedef -----------------------------------------------------------*/
@@ -45,7 +47,11 @@
 
 /* Private variables ---------------------------------------------------------*/
 /* USER CODE BEGIN Variables */
-
+// 任务句柄
+TaskHandle_t taskHandle1;
+TaskHandle_t taskHandle2;
+// 信号量句柄
+QueueHandle_t semphore_handle;
 /* USER CODE END Variables */
 /* Definitions for defaultTask */
 osThreadId_t defaultTaskHandle;
@@ -57,7 +63,8 @@ const osThreadAttr_t defaultTask_attributes = {
 
 /* Private function prototypes -----------------------------------------------*/
 /* USER CODE BEGIN FunctionPrototypes */
-
+void Task1(void *pvParameters);
+void Task2(void *pvParameters);
 /* USER CODE END FunctionPrototypes */
 
 void StartDefaultTask(void *argument);
@@ -80,6 +87,11 @@ void MX_FREERTOS_Init(void) {
 
   /* USER CODE BEGIN RTOS_SEMAPHORES */
   /* add semaphores, ... */
+  // 创建二值信号量
+  semphore_handle = xSemaphoreCreateBinary();
+    if (semphore_handle != NULL){
+        printf("二值信号量创建成功！！！\n");
+    }
   /* USER CODE END RTOS_SEMAPHORES */
 
   /* USER CODE BEGIN RTOS_TIMERS */
@@ -96,6 +108,8 @@ void MX_FREERTOS_Init(void) {
 
   /* USER CODE BEGIN RTOS_THREADS */
   /* add threads, ... */
+    xTaskCreate(Task1,"task1",200,NULL,osPriorityLow,&taskHandle1);
+    xTaskCreate(Task2,"task2",200,NULL,osPriorityLow1,&taskHandle2);
   /* USER CODE END RTOS_THREADS */
 
   /* USER CODE BEGIN RTOS_EVENTS */
@@ -125,6 +139,33 @@ void StartDefaultTask(void *argument)
 
 /* Private application code --------------------------------------------------*/
 /* USER CODE BEGIN Application */
+void Task1(void *pvParameters) {
+    // 任务一:定时释放二值信号量
+    BaseType_t err;
+    while (1){
+        if (semphore_handle != NULL){
+            err = xSemaphoreGive(semphore_handle);
+            if (err == pdPASS){
+                printf("信号量释放成功！\n");
+            } else{
+                printf("信号量释放失败! \n");
+            }
+        }
+        vTaskDelay(5000);
+    }
+}
 
+void Task2(void *pvParameters) {
+    // 任务二:获取二值信号量，当获取成功后打印提示信息
+    BaseType_t err;
+    while (1){
+        err = xSemaphoreTake(semphore_handle,portMAX_DELAY);        // 获取信号量并死等
+        if (err == pdTRUE){
+            printf("信号量获取成功！\n");
+        } else{
+            printf("获取超时 \n");
+        }
+    }
+}
 /* USER CODE END Application */
 
