568caa8686263d8389583ca2a51c0a77aa094517
超级小星星 <62045849+cannnnnnnnnnnn@users.noreply.github.com>
2023-04-10 10:12:38 +0800

    优先级翻转实验-实验现象是还没等到高优先级的任务释放信号量低优先级的任务就获取到了信号量

--- Semaphore_PriorityFlip/Core/Src/app_freertos.c
+++ Semaphore_PriorityFlip/Core/Src/app_freertos.c
@@ -26,6 +26,9 @@
 /* Private includes ----------------------------------------------------------*/
 /* USER CODE BEGIN Includes */
 #include "stdio.h"
+#include "queue.h"
+#include "semphr.h"
+#include "stdlib.h"
 /* USER CODE END Includes */
 
 /* Private typedef -----------------------------------------------------------*/
@@ -45,24 +48,48 @@
 
 /* Private variables ---------------------------------------------------------*/
 /* USER CODE BEGIN Variables */
-
+// 创建任务句柄
+TaskHandle_t taskHandle1;
+TaskHandle_t taskHandle2;
+TaskHandle_t taskHandle3;
+// 信号量
+QueueHandle_t semphore_handle;
 /* USER CODE END Variables */
 /* Definitions for defaultTask */
 osThreadId_t defaultTaskHandle;
 const osThreadAttr_t defaultTask_attributes = {
   .name = "defaultTask",
   .priority = (osPriority_t) osPriorityNormal,
-  .stack_size = 128 * 4
+  .stack_size = 1024
 };
 
 /* Private function prototypes -----------------------------------------------*/
 /* USER CODE BEGIN FunctionPrototypes */
-
+void low_task(void *pvParameters);
+void middle_task(void *pvParameters);
+void high_task(void *pvParameters);
 /* USER CODE END FunctionPrototypes */
 
 void StartDefaultTask(void *argument);
 
 void MX_FREERTOS_Init(void); /* (MISRA C 2004 rule 8.1) */
+
+/* Hook prototypes */
+void configureTimerForRunTimeStats(void);
+unsigned long getRunTimeCounterValue(void);
+
+/* USER CODE BEGIN 1 */
+/* Functions needed when configGENERATE_RUN_TIME_STATS is on */
+__weak void configureTimerForRunTimeStats(void)
+{
+
+}
+
+__weak unsigned long getRunTimeCounterValue(void)
+{
+return 0;
+}
+/* USER CODE END 1 */
 
 /**
   * @brief  FreeRTOS initialization
@@ -80,6 +107,11 @@ void MX_FREERTOS_Init(void) {
 
   /* USER CODE BEGIN RTOS_SEMAPHORES */
   /* add semaphores, ... */
+  // 初始化信号量
+  semphore_handle = xSemaphoreCreateBinary();
+    if (semphore_handle != NULL){
+        printf("二值信号量创建成功！\n");
+    }
   /* USER CODE END RTOS_SEMAPHORES */
 
   /* USER CODE BEGIN RTOS_TIMERS */
@@ -96,6 +128,9 @@ void MX_FREERTOS_Init(void) {
 
   /* USER CODE BEGIN RTOS_THREADS */
   /* add threads, ... */
+    xTaskCreate(low_task,"low_task",200,NULL,osPriorityLow,&taskHandle1);
+    xTaskCreate(middle_task,"middle_task",200,NULL,osPriorityLow1,&taskHandle2);
+    xTaskCreate(high_task,"high_task",200,NULL,osPriorityLow2,&taskHandle3);
   /* USER CODE END RTOS_THREADS */
 
   /* USER CODE BEGIN RTOS_EVENTS */
@@ -115,8 +150,14 @@ void StartDefaultTask(void *argument)
 {
   /* USER CODE BEGIN StartDefaultTask */
   /* Infinite loop */
+  char *str_buffer;
   for(;;)
   {
+//      str_buffer = malloc(500);
+//      vTaskList(str_buffer);
+//      printf("任务名\t\t\t状态\t优先级\t剩余栈\t任务序号\r\n");
+//      printf("%s\r\n", str_buffer);
+//      free(str_buffer);
       HAL_GPIO_TogglePin(LD2_GPIO_Port,LD2_Pin);
     osDelay(500);
   }
@@ -125,6 +166,36 @@ void StartDefaultTask(void *argument)
 
 /* Private application code --------------------------------------------------*/
 /* USER CODE BEGIN Application */
+void low_task(void *pvParameters) {
+    while (1){
+        printf("low_task获取信号量\n");
+        xSemaphoreTake(semphore_handle,portMAX_DELAY);
+        printf("low_task正在运行\n");
+        HAL_Delay(3000);
+        printf("low_task释放信号量\n");
+        xSemaphoreGive(semphore_handle);
+        vTaskDelay(1000);
+    }
+}
 
+void middle_task(void *pvParameters) {
+    while (1){
+        printf("middle_task正在运行\n");
+        vTaskDelay(1000);
+    }
+}
+
+void high_task(void *pvParameters) {
+    while (1){
+        printf("high_task获取信号量\n");
+        xSemaphoreTake(semphore_handle,portMAX_DELAY);
+        printf("high_task正在运行\n");
+        HAL_Delay(1000);
+        printf("high_task释放信号量\n");
+        xSemaphoreGive(semphore_handle);
+        vTaskDelay(1000);
+
+    }
+}
 /* USER CODE END Application */
 
